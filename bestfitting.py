a47e25e0d14db28f6381eca904c5ecf047435cde
shinjam2912 <shinbebdec@gmail.com>
2026-02-09 21:41:58 +0530

    Add files via upload

---
+++ bestfitting.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+points = [(1, 2), (2, 2.8), (3, 3.6), (4, 4.5), (5, 5.1)]
+#points=eval(input("Enter a list of coordinates"))
+x=[i[0] for i in points]
+y=[i[1] for i in points]
+n=len(points)
+
+m=(n*sum(x[i]*y[i] for i in range(n)) - (sum(x)*sum(y)))/(n*sum(x[i]**2 for i in range(n)) - sum(x)**2)
+c=(sum(y)-m*sum(x))/n
+
+print("y =",m,"x+",c)
+xp=np.linspace(min(x),max(x),100)
+yp=m*xp+c
+
+plt.scatter(x,y)
+plt.plot(xp,yp)
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.title("BEst fitting line")
+plt.show()
